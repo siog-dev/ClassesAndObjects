c6d71149eb0b5fea90780b36aff1246b3eb9306c
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-12 21:07:29 +0000

    add instance variables to the Student class

--- ClassesAndObjects/src/ie/atu/sw/Student.java
+++ ClassesAndObjects/src/ie/atu/sw/Student.java
@@ -1,5 +1,13 @@
 package ie.atu.sw;
 
 public class Student {
-
+	
+	/*
+	 * Define some state for the class Student
+	 * Public visibility modifier used for example purposes ONLY
+	 */
+	
+	public long sid;
+	public String name = "Undefined";//not a great name, for example purposes
+	
 }
