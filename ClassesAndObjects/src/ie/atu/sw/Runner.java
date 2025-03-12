0847e61750a10c70475c67333001e232f6697314
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-12 22:02:29 +0000

    amend comments and remove study notes for code readability

--- ClassesAndObjects/src/ie/atu/sw/Runner.java
+++ ClassesAndObjects/src/ie/atu/sw/Runner.java
@@ -5,10 +5,7 @@ import java.time.LocalDate;
 public class Runner {
 	
 	public static void main(String[] args) {
-		/*
-		 * Student is our variable, it's a type, it has an identifier "s"
-		 * it is assigned a legal value for it's type: "new Student()"
-		 */
+
 		Student s = new Student(); //Use the new keyword against a constructor
 		
 		/*
@@ -17,7 +14,9 @@ public class Runner {
 		 */
 		
 		System.out.println("Student Object ID from Runner: " + s);
-		s.sid = 99999; //Use dot notation to access instance variables (ctrl + space)
+		
+		//Use the dot notation to access features
+		s.sid = 99999;
 		s.name = "Joe Murphy";
 		s.dob = LocalDate.now();
 	}
