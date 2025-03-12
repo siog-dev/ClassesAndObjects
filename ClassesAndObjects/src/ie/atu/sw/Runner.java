09f7fc7fe40df0809e3e711ebc78d80f199a02c9
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-12 21:46:08 +0000

    amend comments for clarity

--- ClassesAndObjects/src/ie/atu/sw/Runner.java
+++ ClassesAndObjects/src/ie/atu/sw/Runner.java
@@ -4,10 +4,8 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		/*
-		 * Student is our variable, it's a type
-		 * it has an identifier "s"
-		 * it is assigned a legal value for it's type:
-		 * "new Student()"
+		 * Student is our variable, it's a type, it has an identifier "s"
+		 * it is assigned a legal value for it's type: "new Student()"
 		 */
 		Student s = new Student(); //Use the new keyword against a constructor
 		
