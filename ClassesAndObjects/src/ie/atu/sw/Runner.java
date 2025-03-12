979f75d4c7d4c070193469a8b86f8b65333becf0
siog <siog@LAPTOP-545TJ6PE.station>
2025-03-12 20:29:17 +0000

    create a new instance of the Student object and add a print statement

--- ClassesAndObjects/src/ie/atu/sw/Runner.java
+++ ClassesAndObjects/src/ie/atu/sw/Runner.java
@@ -3,6 +3,15 @@ package ie.atu.sw;
 public class Runner {
 	
 	public static void main(String[] args) {
+		/*
+		 * Student is our variable, it's a type
+		 * it has an identifier "s"
+		 * it is assigned a legal value for it's type:
+		 * "new Student()"
+		 */
+		Student s = new Student();
+		
+		System.out.println("Student from Runner: " + s);//s is the student identifier
 		
 	}
 	
