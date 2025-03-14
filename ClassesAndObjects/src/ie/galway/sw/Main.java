97216454d7e7ff1d4a01bd72aa29ee773cd81ace
siog <siog@LAPTOP-545TJ6PE>
2025-03-14 15:47:50 +0000

    use dot notation to access methods/instance variables from Student class
    protected methods in ie.atu.sw package are not
    visible in the ie.galway.sw package

--- ClassesAndObjects/src/ie/galway/sw/Main.java
+++ ClassesAndObjects/src/ie/galway/sw/Main.java
@@ -6,6 +6,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Student s = new Student();
+		s.study(); //Public method(behavior) "study" accessible
+		System.out.println(s.registered); //Public instance variable(state) "registered" accessible
+		s.register() //Protected method(behavior) NOT accessible
 	}
 
 }
